773e7119136373d5baabebbf36261d6f31eccc48
TextError <savinraul1983@yahoo.com>
2019-02-18 16:44:47 +0000

    test viewset

--- src/project/profiles_api/views.py
+++ src/project/profiles_api/views.py
@@ -52,6 +52,9 @@ class HelloApiView(APIView):
 
 class HelloViewSet(viewsets.ViewSet):
   """Test API ViewSet"""
+
+  serializer_class = serializers.HelloSerializer
+
   def list(selt, request):
     """Return a Hello message"""
     a_viewset = [
@@ -61,3 +64,31 @@ class HelloViewSet(viewsets.ViewSet):
     ]
 
     return Response({ 'message': 'Hello', 'a_viewset': a_viewset })
+
+  def create(self, request):
+    """Create a new hello message"""
+    
+    serializer = serializers.HelloSerializer(data=request.data)
+
+    if serializers.is_valid():
+      name = serializers.data.get('name')
+      message = 'Hello {0}'.format(name)
+      return Response({ 'message': message })
+    else:
+      return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)
+
+  def retrieve(self, request, pk=None):
+    """Handles getting an object by its ID"""
+    return Response({'http_method': 'GET'})
+
+  def update(self, request, pk=None):
+    """Handles updating an object"""
+    return Response({'http_method': 'PUT'})
+
+  def partial_update(self, request, pk=None):
+    """Handles update part of an object"""
+    return Response({'http_method': 'PATCH'})
+  
+  def destroy(self, request, pk:None):
+    """Handles removing an object"""
+    return Response({'http_method': 'DELETE'})
