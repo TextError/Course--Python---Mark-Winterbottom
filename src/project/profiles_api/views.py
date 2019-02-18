78effc018f740eb8363ed888ebdc3c84069f7987
TextError <savinraul1983@yahoo.com>
2019-02-18 17:29:51 +0000

    testig profile create

--- src/project/profiles_api/views.py
+++ src/project/profiles_api/views.py
@@ -6,6 +6,7 @@ from rest_framework import status
 from rest_framework import viewsets
 
 from . import serializers
+from . import models
 
 
 # Create your views here.
@@ -92,3 +93,9 @@ class HelloViewSet(viewsets.ViewSet):
   def destroy(self, request, pk:None):
     """Handles removing an object"""
     return Response({'http_method': 'DELETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+  """Handle creating, reading and updating profiles"""
+  serializer_class = serializers.UserProfileSerializer
+  queryset = models.UserProfile.object.all()
