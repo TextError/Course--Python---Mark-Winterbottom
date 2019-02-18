a3a28f05f06781a5e697f10ca1a8a460939efda9
TextError <savinraul1983@yahoo.com>
2019-02-18 17:46:02 +0000

    added permission

--- src/project/profiles_api/views.py
+++ src/project/profiles_api/views.py
@@ -4,9 +4,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from . import serializers
 from . import models
+from . import permission
 
 
 # Create your views here.
@@ -99,3 +101,5 @@ class UserProfileViewSet(viewsets.ModelViewSet):
   """Handle creating, reading and updating profiles"""
   serializer_class = serializers.UserProfileSerializer
   queryset = models.UserProfile.object.all()
+  authentication_classes = (TokenAuthentication,)
+  permission_classes = (permission.UpdateOwnProfile,)
